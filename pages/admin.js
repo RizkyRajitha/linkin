2c4c3c5802d88557f1eff190e7e675d40eca29fd
rizkyrajitha <rajithagunathilake@gmail.com>
2021-05-11 17:02:27 +0530

    end point env variable

--- pages/admin.js
+++ pages/admin.js
@@ -7,11 +7,11 @@ import { useState } from "react";
 import { cookieValidateLogin } from "../middleware/middleware";
 const endpoint =
   process.env.NODE_ENV === "production"
-    ? `${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : "http://localhost:3000";
 
 console.log(endpoint);
-console.log(process.env.VERCEL_URL);
+// console.log(process.env.VERCEL_URL);
 export async function getServerSideProps({ req, res }) {
   try {
     cookieValidateLogin(req, res);
