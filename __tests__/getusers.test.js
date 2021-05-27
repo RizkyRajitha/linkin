f3160c94a97dc3cf2585532ac2534472b5992863
rizkyrajitha <rajithagunathilake@gmail.com>
2021-05-27 23:34:43 +0530

    added getUser test

--- __tests__/getusers.test.js
+++ __tests__/getusers.test.js
@@ -1,38 +1,13 @@
-const { Client } = require("pg");
-
-const connectionString =
-  process.env.NODE_ENV === "production" ? process.env.DBURL : DBURLLOCAL; //require("../config/config").DBURL; //'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
-
-const client = new Client({
-  connectionString: connectionString,
-});
-
 const { getUser } = require("../lib/dbfunc");
 
-console.log(process.env);
-console.log(connectionString);
-
-beforeAll(async () => {
-  // Clears the database and adds some testing data.
-  // Jest will wait for this promise to resolve before running tests.
-
-  await client.connect();
-  console.log("connected to database");
-
-  console.log(await getUser());
-
-  //   let script = ``
-
-  //   console.log(script);
-
-  //   let query = {
-  //     text: script,
-  //     values: [],
-  //   };
-});
-
-describe("Get users from database ", () => {
-  it(" '' is true", () => {
-    expect(isEmptry("")).toBe(true);
+describe("Get users from database ", async () => {
+  it(" initnial admin is succesfull inserted", () => {
+    let { user } = await getUser();
+    const expectedUser = {
+      username: "admin",
+      password: "$2b$10$gKoU.xdV9vrGY2wEW0KAnuBmQeYxOUgXRHS9f8Sgx40m7kxpejddG",
+    };
+    expect(user).toMatchObject(expectedUser);
+    // expect(isEmptry("")).toBe(true);
   });
 });
