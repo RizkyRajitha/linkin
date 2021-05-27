589f7cdae11d6c55410f8f07950ee9148e53d8d1
rizkyrajitha <rajithagunathilake@gmail.com>
2021-05-27 23:41:46 +0530

    added test for get admin user

--- __tests__/getusers.test.js
+++ __tests__/getusers.test.js
@@ -1,13 +1,10 @@
 const { getUser } = require("../lib/dbfunc");
 
-describe("Get users from database ", async () => {
-  it(" initnial admin is succesfull inserted", () => {
-    let { user } = await getUser();
-    const expectedUser = {
-      username: "admin",
-      password: "$2b$10$gKoU.xdV9vrGY2wEW0KAnuBmQeYxOUgXRHS9f8Sgx40m7kxpejddG",
-    };
-    expect(user).toMatchObject(expectedUser);
-    // expect(isEmptry("")).toBe(true);
-  });
+test("get admin user", async () => {
+  let user = await getUser("admin");
+  const expectedUser = {
+    username: "admin",
+    password: "$2b$10$gKoU.xdV9vrGY2wEW0KAnuBmQeYxOUgXRHS9f8Sgx40m7kxpejddG",
+  };
+  expect(user).toMatchObject(expectedUser);
 });
