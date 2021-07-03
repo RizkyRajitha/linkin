5dc365f99a15e2b6148d628fc2b622a1805d88cc
rizkyrajitha <rajithagunathilake@gmail.com>
2021-07-03 15:44:31 +0530

    added prisma with globle object fro export

--- db/dbconprisma.js
+++ db/dbconprisma.js
@@ -1,4 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+
+  prisma = global.prisma;
+}
 
 export default prisma;
