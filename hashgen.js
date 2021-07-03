fffecd1366277892e154dbd33e20fdf3b7ca0e79
rizkyrajitha <rajithagunathilake@gmail.com>
2021-07-03 22:35:47 +0530

    error

--- hashgen.js
+++ hashgen.js
@@ -1,9 +1,9 @@
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 
-const saltRounds = 10;
-const myPlaintextPassword = "linkin123";
+// const saltRounds = 10;
+// const myPlaintextPassword = "linkin123";
 
-const salt = bcrypt.genSaltSync(saltRounds);
-const hash = bcrypt.hashSync(myPlaintextPassword, salt);
-console.log(hash);
+// const salt = bcrypt.genSaltSync(saltRounds);
+// const hash = bcrypt.hashSync(myPlaintextPassword, salt);
+// console.log(hash);
 
