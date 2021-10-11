0f61106c5c1b72aa17957889a74cb4e7612df44e
dubisdev <dubisdev@gmail.com>
2021-10-11 16:00:31 +0200

    Feat: Add social icons layout

--- components/linkinthebiopage.js
+++ components/linkinthebiopage.js
@@ -2,103 +2,127 @@ import { isEmpty } from "../lib/side";
 // import Image from "next/image";
 
 export default function Home({
-  handlerText,
-  bgColor,
-  handlerLink,
-  avatarUrl,
-  accentColor,
-  avatarwidth,
-  avatarBorderColor,
-  handlerFontSize,
-  handlerFontColor,
-  fontFamily,
-  fontUrl,
-  linkData,
-  footerText,
-  footerTextSize,
-  footerBgColor,
-  footerTextColor,
-  handlerDescriptionFontColor,
-  handlerDescription,
-  bgImgUrl,
-  footerEnabled,
-  linkPadding,
-  linktreeWidth,
-  preview = false,
+	handlerText,
+	bgColor,
+	handlerLink,
+	avatarUrl,
+	accentColor,
+	avatarwidth,
+	avatarBorderColor,
+	handlerFontSize,
+	handlerFontColor,
+	fontFamily,
+	fontUrl,
+	linkData,
+	footerText,
+	footerTextSize,
+	footerBgColor,
+	footerTextColor,
+	handlerDescriptionFontColor,
+	handlerDescription,
+	bgImgUrl,
+	footerEnabled,
+	linkPadding,
+	linktreeWidth,
+	preview = false,
 }) {
-  accentColor = isEmpty(accentColor) ? "#BDD7FF" : accentColor;
-  avatarwidth = isEmpty(avatarwidth) ? "50" : avatarwidth;
-  avatarBorderColor = isEmpty(avatarBorderColor) ? "#fff" : avatarBorderColor;
-  handlerFontSize = isEmpty(handlerFontSize) ? "15" : handlerFontSize;
-  handlerFontColor = isEmpty(handlerFontColor) ? "#fff" : handlerFontColor;
-  bgColor = isEmpty(bgColor) ? "#fff" : bgColor;
-  fontFamily = isEmpty(fontFamily) ? "'Roboto', sans-serif" : fontFamily;
-  fontUrl = isEmpty(fontUrl)
-    ? "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-    : fontUrl;
-  footerTextSize = isEmpty(footerTextSize) ? 12 : footerTextSize;
-  footerBgColor = isEmpty(footerBgColor) ? "#000000" : footerBgColor;
-  footerTextColor = isEmpty(footerTextColor) ? "#ffffff" : footerTextColor;
-  linkPadding = isEmpty(linkPadding) ? "2em" : `${linkPadding}em`;
-  linktreeWidth = isEmpty(linktreeWidth) ? "320px" : `${linktreeWidth}px`;
+	accentColor = isEmpty(accentColor) ? "#BDD7FF" : accentColor;
+	avatarwidth = isEmpty(avatarwidth) ? "50" : avatarwidth;
+	avatarBorderColor = isEmpty(avatarBorderColor) ? "#fff" : avatarBorderColor;
+	handlerFontSize = isEmpty(handlerFontSize) ? "15" : handlerFontSize;
+	handlerFontColor = isEmpty(handlerFontColor) ? "#fff" : handlerFontColor;
+	bgColor = isEmpty(bgColor) ? "#fff" : bgColor;
+	fontFamily = isEmpty(fontFamily) ? "'Roboto', sans-serif" : fontFamily;
+	fontUrl = isEmpty(fontUrl)
+		? "https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+		: fontUrl;
+	footerTextSize = isEmpty(footerTextSize) ? 12 : footerTextSize;
+	footerBgColor = isEmpty(footerBgColor) ? "#000000" : footerBgColor;
+	footerTextColor = isEmpty(footerTextColor) ? "#ffffff" : footerTextColor;
+	linkPadding = isEmpty(linkPadding) ? "2em" : `${linkPadding}em`;
+	linktreeWidth = isEmpty(linktreeWidth) ? "320px" : `${linktreeWidth}px`;
 
-  return (
-    <div>
-      <div className="outterwrap">
-        <div className="wrap">
-          <div className="profile">
-            {!isEmpty(avatarUrl) && <img src={avatarUrl} className="photo" />}
-            <a
-              className="handlerLink"
-              href={`${handlerLink || "#"}`}
-              target="_blank"
-            >
-              <span className="handlerText">{handlerText}</span>
-            </a>
-            <p className="handlerDescription">{handlerDescription}</p>
-          </div>
-          <div className="links">
-            <ul>
-              {linkData.map((link, id) => {
-                return (
-                  <li key={id}>
-                    <a
-                      href={`${link.linkUrl || "#"}`}
-                      className="link"
-                      target="_blank"
-                      style={{
-                        backgroundColor: link.bgColor || "#2c6bed",
-                        color: link.textColor || "#ffffff",
-                        borderRadius: `${link.borderRadius || "4"}px`,
-                      }}
-                    >
-                      {link.iconClass && (
-                        <i className={`${link.iconClass} icon`}></i>
-                      )}
-                      <div className="d-flex w-100 align-items-center justify-content-center">
-                        {link.displayText}
-                      </div>
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </div>
-      </div>
-      {footerEnabled && (
-        <div className="footer d-flex align-items-center justify-content-center">
-          {/* Copyright © 2021 All Rights Reserved by. */}
-          {footerText}
-        </div>
-      )}
+	return (
+		<div>
+			<div className="outterwrap">
+				<div className="wrap">
+					<div className="profile">
+						{!isEmpty(avatarUrl) && <img src={avatarUrl} className="photo" />}
+						<a
+							className="handlerLink"
+							href={`${handlerLink || "#"}`}
+							target="_blank"
+						>
+							<span className="handlerText">{handlerText}</span>
+						</a>
+						<p className="handlerDescription">{handlerDescription}</p>
+					</div>
+					<div className="social">
+						<ul>
+							{/* {[...linkData, ...linkData, ...linkData].map((element, id) => { */}
+							{linkData.map((link, id) => {
+								return (
+									<li key={id}>
+										<a
+											href={`${link.linkUrl || "#"}`}
+											className="social_icon"
+											target="_blank"
+											style={{
+												backgroundColor: link.bgColor || "#2c6bed",
+												color: link.textColor || "#ffffff",
+												borderRadius: `${link.borderRadius || "4"}px`,
+											}}>
+											{link.iconClass && (
+												<i className={`${link.iconClass} single_icon`}></i>
+											)}
+										</a>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+					<div className="links">
+						<ul>
+							{linkData.map((link, id) => {
+								return (
+									<li key={id}>
+										<a
+											href={`${link.linkUrl || "#"}`}
+											className="link"
+											target="_blank"
+											style={{
+												backgroundColor: link.bgColor || "#2c6bed",
+												color: link.textColor || "#ffffff",
+												borderRadius: `${link.borderRadius || "4"}px`,
+											}}
+										>
+											{link.iconClass && (
+												<i className={`${link.iconClass} icon`}></i>
+											)}
+											<div className="d-flex w-100 align-items-center justify-content-center">
+												{link.displayText}
+											</div>
+										</a>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+			</div>
+			{footerEnabled && (
+				<div className="footer d-flex align-items-center justify-content-center">
+					{/* Copyright © 2021 All Rights Reserved by. */}
+					{footerText}
+				</div>
+			)}
 
-      <style
-        jsx
-        onError={(e) => {
-          console.log(e);
-        }}
-      >{`
+			<style
+				jsx
+				onError={(e) => {
+					console.log(e);
+				}}
+				>{`
         @import url("${fontUrl}");
 
         @import url("https://use.fontawesome.com/releases/v5.8.1/css/all.css");
@@ -180,6 +204,35 @@ export default function Home({
           font-size: ${handlerFontSize}px;
         }
 
+        .social {
+			margin: 0 -2rem 0 -2rem;
+		}
+		.social ul {
+			list-style: none;
+			padding: 0;
+			margin: 0 auto;
+			width: fit-content;
+			display: flex;
+			flex-wrap: wrap;
+			align-items: center;
+			justify-content: center;
+		}
+		.social ul li {
+			margin: 7px;
+		}
+		.social_icon {
+			display: inline-block;
+			padding: 0.5rem;
+			text-align: center;
+			text-decoration: none;
+			border-radius: 4px;
+			transition: ease all 0.3s;
+			color: #fff;
+			align-items: center;
+		}
+		.social_icon:hover {
+			opacity: 0.9;
+		}
         .links ul {
           list-style: none;
           padding: 0;
@@ -206,13 +259,18 @@ export default function Home({
           // padding: 1rem;
           font-size: 1.7rem;
         }
+
+        .single_icon {
+			font-size: 2rem;
+		}
+
         @media (max-width: 768px) {
           .link {
             padding: 1.2rem;
           }
       `}</style>
-    </div>
-  );
+		</div>
+	);
 }
 //
 
