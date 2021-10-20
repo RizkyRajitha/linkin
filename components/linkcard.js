840aec1d58a849c1c54a70e73a3e768576698dfc
dubisdev <dubisdev@gmail.com>
2021-10-21 01:04:13 +0200

    fix previous link regex pattern

--- components/linkcard.js
+++ components/linkcard.js
@@ -125,8 +125,9 @@ export default function LinkCard({
                       {...register(`linkUrl`, {
                         pattern: {
                           message: "Should be a valid URL",
+                          // see https://regexr.com/67tue
                           value:
-                            /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
+                            /(https?:\/\/)?(www\.)?(([a-zA-Z0-9-\._]+(\.[a-zA-Z]{2,}(:[0-9]+)?(\/)?)$)|(([a-zA-Z0-9-\._]+(\.[a-zA-Z]{2,}(:[0-9]+)?)(\/[a-zA-Z0-9-\._]+)+)(\.[a-zA-Z]{2,})?(\/+)?))/i,
                         },
                       })}
                     />
