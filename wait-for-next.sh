2c42ec40ef5a94e5ef11fdd1b00a5225566788f7
rizkyrajitha <rajithagunathilake@gmail.com>
2021-11-21 17:36:06 +0530

    added wait-for-next.sh file to run server

---
+++ wait-for-next.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+npm run dev  &
+APP_PID=$!
+
+echo $APP_PID
+
+echo "next lunching 3000..."
+
+while ! nc -z localhost 3000; do   
+  sleep 0.1
+done
+
+echo "run pupperteer"
+node browser.js
+
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo pupperteer success
+else
+  echo pupperteer failed
+fi
+
+ls
+
+pkill $APP_PID
+
+
+
