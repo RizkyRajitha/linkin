576ea2a32cd2d95b3dd6a909a6a6ed19689e26a9
rizkyrajitha <rajithagunathilake@gmail.com>
2021-11-22 12:19:06 +0530

    debug set output

--- scripts/browser.js
+++ scripts/browser.js
@@ -42,7 +42,7 @@ const run = async () => {
 
     await browser.close();
 
-    let commentBody = "";
+    let commentBody = "Screenshots ";
 
     urlList.forEach((element) => {
       commentBody = commentBody + ` ![image](${element}) \n\n`;
@@ -52,7 +52,7 @@ const run = async () => {
     // execSync(
     //   `echo "action_state=![image](${uplaodedImage.url})" >> $GITHUB_ENV`
     // );
-    execSync(`echo "commentBody=Screenshots ${commentBody}" >> $GITHUB_ENV`);
+    // execSync(`echo "commentBody=Screenshots ${commentBody}" >> $GITHUB_ENV`);
     execSync(
       `echo '::set-output name=commentBody::Screenshots via set out ${commentBody}'`
     );
