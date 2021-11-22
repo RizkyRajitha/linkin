cf5408c6d4d5b9de8a78d1796b188876adbe4cd3
rizkyrajitha <rajithagunathilake@gmail.com>
2021-11-22 15:14:45 +0530

    added devices for browser testign and added new resolution tetsing

--- scripts/browser-test.js
+++ scripts/browser-test.js
@@ -24,7 +24,8 @@ cloudinary.config({
 let browser;
 
 let cwd = path.resolve(__dirname);
-console.log(cwd);
+
+// not checking if the dir exists because this only run in ci environment
 fs.mkdirSync(`${cwd}/images`);
 
 const run = async () => {
@@ -33,10 +34,21 @@ const run = async () => {
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
-    console.log("captureIndexPage");
+    console.log("captureIndexPage resolution 1920X1080");
+
     await captureIndexPage(browser);
-    console.log("captureDashboard");
+
+    console.log("captureIndexPage iphone 12/13 pro resolution 390X844");
+
+    await captureIndexPage(
+      browser,
+      { height: 844, width: 390 },
+      "iphone 12/13"
+    );
+
+    console.log("captureDashboard resolution 1920X1080");
     await captureDashboard(browser);
+
     console.log("uplaodImages");
     let urlList = await uplaodImages();
 
@@ -65,14 +77,15 @@ const run = async () => {
 run();
 
 const uplaodImages = async () => {
-  images = fs.readdirSync(`${cwd}/images/`);
-  console.log(images);
-
   let promiseArray = [];
   let urlList = [];
 
-  images.forEach(async (element) => {
-    console.log("upload image..");
+  //read images in screenshot distination directory
+  let images = fs.readdirSync(`${cwd}/images/`);
+
+  images.forEach((element) => {
+    console.log("uploading image to cloudinary..");
+
     let uplaodedImagePromise = cloudinary.uploader.upload(
       `${cwd}/images/${element}`,
       {
@@ -86,7 +99,7 @@ const uplaodImages = async () => {
   });
 
   urlList = await Promise.all(promiseArray);
-  console.log(urlList);
+
   urlList = urlList.map((ele) => {
     let ssname = String(ele.original_filename).split("-")[1];
     return {
@@ -94,17 +107,18 @@ const uplaodImages = async () => {
       ssname: ssname,
     };
   });
-  console.log(urlList);
+
   return urlList;
 };
 
 const captureIndexPage = async (
   browser,
-  viewport = { width: 1920, height: 1080 }
+  viewport = { width: 1920, height: 1080 },
+  deviceName = ""
 ) => {
   let page = await browser.newPage();
 
-  const ssname = `Index ${viewport.width}X${viewport.height}`;
+  const ssname = `Index ${deviceName} ${viewport.width}X${viewport.height}`;
 
   await page.setViewport(viewport);
   await page.goto(testingUrl, {
@@ -118,9 +132,10 @@ const captureIndexPage = async (
 
 const captureDashboard = async (
   browser,
-  viewport = { width: 1920, height: 1080 }
+  viewport = { width: 1920, height: 1080 },
+  deviceName = ""
 ) => {
-  const ssname = `Dashboard ${viewport.width}X${viewport.height}`;
+  const ssname = `Index ${deviceName} ${viewport.width}X${viewport.height}`;
 
   let page = await browser.newPage();
 
@@ -130,9 +145,11 @@ const captureDashboard = async (
     waitUntil: "networkidle2",
   });
 
+  // login to dashboard using default credentials
   await page.type("#username", "admin");
   await page.type("#password", "linkin123");
 
+  // wait till login to dashbaord
   await Promise.all([
     page.click("#submit"),
     page.waitForNavigation({ waitUntil: "networkidle2" }),
