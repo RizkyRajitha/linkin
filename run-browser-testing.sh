82534eb37cdee94096ecee8f90811a1708d17e33
rizkyrajitha <rajithagunathilake@gmail.com>
2021-11-22 15:01:26 +0530

    minor refactors, uncomment coverage testing

--- run-browser-testing.sh
+++ run-browser-testing.sh
@@ -8,13 +8,15 @@ echo $APP_PID
 
 echo "next lunching 3000..."
 
+# wait till the server starts
 while ! nc -z localhost 3000; do   
   sleep 0.1
 done
 
-echo "run pupperteer"
-node scripts/browser.js
+echo "run pupperteer browser testing..."
+node scripts/browser-test.js
 
+# exit if pupperteer script failed
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
   echo pupperteer success
@@ -22,9 +24,8 @@ else
   echo pupperteer failed
 fi
 
-kill $APP_PID
-
-ls
+# end server process
+# kill $APP_PID
 
 
 
