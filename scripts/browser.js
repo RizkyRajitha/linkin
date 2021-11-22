58a676727e03ab330a12453d7036f62c9d4ed38f
rizkyrajitha <rajithagunathilake@gmail.com>
2021-11-22 09:24:34 +0530

    update ghactions

--- scripts/browser.js
+++ scripts/browser.js
@@ -6,19 +6,19 @@ const cloudinary = require("cloudinary").v2;
 
 console.log(process.env.CLOUDINARY_CLOUD_NAME);
 
-if (
-  !process.env.CLOUDINARY_CLOUD_NAME &&
-  !process.env.CLOUDINARY_API_KEY &&
-  !process.env.CLOUDINARY_API_SECRET
-) {
-  console.log("cloudinary not configured");
-  process.exit(0);
-}
+// if (
+//   !process.env.CLOUDINARY_CLOUD_NAME &&
+//   !process.env.CLOUDINARY_API_KEY &&
+//   !process.env.CLOUDINARY_API_SECRET
+// ) {
+//   console.log("cloudinary not configured");
+//   process.exit(0);
+// }
 
 cloudinary.config({
-  cloud_name: process.env.cloud_name,
-  api_key: process.env.api_key,
-  api_secret: process.env.api_secret,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 let browser;
