2790860be6fd7ed64abe1bf546be940f79a2673a
dubisdev <dubisdev@gmail.com>
2021-12-02 22:05:26 +0100

    refactor: extract SocialIconsSection component

--- components/linktree.js
+++ components/linktree.js
@@ -1,4 +1,5 @@
 import { isEmpty, isHex } from "../lib/side";
+import SocialIconsSection from "./linktreesocialsection";
 // import Image from "next/image";
 
 export default function Home({
@@ -26,6 +27,7 @@ export default function Home({
   linkPadding,
   linktreeWidth,
   preview = false,
+  iconsPosition,
 }) {
   let linkPaddingLowWidth = isEmpty(linkPadding)
     ? "2em"
@@ -65,32 +67,10 @@ export default function Home({
 
             <p className="handlerDescription">{handlerDescription}</p>
           </div>
-          <div className="social">
-            <ul>
-              {socialData.map((link, id) => {
-                return (
-                  <li key={id}>
-                    <a
-                      href={`${link.linkUrl || "#"}`}
-                      className="social_icon"
-                      target="_blank"
-                      style={{
-                        backgroundColor: link.bgColor || "#2c6bed",
-                        color: link.textColor || "#ffffff",
-                        borderRadius: `${link.borderRadius || "4"}px`,
-                      }}
-                    >
-                      {link.iconClass && (
-                        <i
-                          className={`${link.iconClass} single_icon fa-fw`}
-                        ></i>
-                      )}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          {iconsPosition === "top" && (
+            <SocialIconsSection socialData={socialData} />
+          )}
+
           <div className="links">
             <ul>
               {linkData.map((link, id) => {
@@ -118,7 +98,11 @@ export default function Home({
               })}
             </ul>
           </div>
+          {iconsPosition === "bottom" && (
+            <SocialIconsSection socialData={socialData} />
+          )}
         </div>
+
         {footerEnabled && (
           <div className="footer d-flex align-items-center justify-content-center">
             {/* Copyright © 2021 All Rights Reserved by. */}
@@ -219,39 +203,6 @@ export default function Home({
           font-size: ${handlerFontSize}px;
         }
 
-        .social {
-          // margin: 0 -2rem 0 -2rem;
-        }
-        .social ul {
-          list-style: none;
-          padding: 0;
-          margin: 0 auto;
-          width: fit-content;
-          display: flex;
-          flex-wrap: wrap;
-          align-items: center;
-          justify-content: center;
-        }
-
-        .social ul li {
-          margin: 7px;
-        }
-
-        .social_icon {
-          display: inline-flex;
-          padding: 0.5rem;
-          text-align: center;
-          text-decoration: none;
-          border-radius: 4px;
-          transition: ease all 0.3s;
-          color: #fff;
-          align-items: center;
-        }
-
-        .social_icon:hover {
-          opacity: 0.9;
-        }
-
         .links ul {
           list-style: none;
           padding: 0;
@@ -278,9 +229,7 @@ export default function Home({
           font-size: 1.7rem;
         }
 
-        .single_icon {
-          font-size: 2rem;
-        }
+        
 
         @media (max-width: 768px) {
           .link {
